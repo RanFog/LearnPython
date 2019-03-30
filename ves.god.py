6ae4ebe9babdc53e89f4c29d62f35cb3a1b01336
Constantin Tischenko <constish@gmail.com>
2019-03-30 12:06:57 +0300

    2para

--- ves.god.py
+++ ves.god.py
@@ -1,3 +1,8 @@
-year = 2000
+year = input("Введите год: ")
+year = int(year)
+print(year)
 is_leap = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 print(is_leap)
+
+import calendar
+print (calendar.isleap(1980))
