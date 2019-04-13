c9500d1c3fb9ebb296be3e7bd49cc718d760c339
Constantin Tischenko <constish@gmail.com>
2019-04-13 09:47:37 +0300

    Style

--- DZ/dz1.py
+++ DZ/dz1.py
@@ -1,8 +1,11 @@
 import random
 secret_number = random.randint(0,100) #A ≤ N ≤ B
-__EXIT__ = -1000;
-answer = -100;
+
+__EXIT__ = -1000
+answer = -100
+
 print ("Попробуйте отгадать загаданное число! (Для выхода наберите: ", __EXIT__,")")
+
 while answer != __EXIT__ :
     print("---------------------------\nВведите число:")
     try:
@@ -10,6 +13,7 @@ while answer != __EXIT__ :
     except Exception as ex:
         print("Надо вводить числа!")
         continue
+
     if answer == secret_number : 
         print ("Вы угадали!") 
         break
